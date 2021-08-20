1ba5b567f089f83ba1b4356df567900e3ea22362
NekoChan <aaron74327@gmail.com>
2021-08-19 23:33:21 -0700

    rename

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const loopAction_1 = require("./loopAction");
 const { version } = require('../package.json');
-process.title = `mkvmerge-font-subtitle v${version} By NekoChan#2851 - Nyahello ~`;
+process.title = `auto-mkvmerge v${version} By NekoChan#2851 - Nyaharo ~`;
 loopAction_1.loopAction();
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { loopAction } from './loopAction'
 
 const { version } = require('../package.json')
 
-process.title = `mkvmerge-font-subtitle v${version} By NekoChan#2851 - Nyahello ~`
+process.title = `auto-mkvmerge v${version} By NekoChan#2851 - Nyaharo ~`
 
 loopAction()
